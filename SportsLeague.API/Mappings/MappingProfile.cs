543c415203fb3cab396dff228d88b95f4b5b0860
Jhon Eyder Giraldo <exneij27@gmail.com>
2026-03-29 17:36:04 -0500

    Add project files.

---
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SportsLeague.API.DTOs.Request;
+using SportsLeague.API.DTOs.Response;
+using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.API.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Team mappings
+            CreateMap<TeamRequestDTO, Team>();
+            CreateMap<Team, TeamResponseDTO>();
+
+            // Aquí puedes agregar más mapeos para otras entidades como Player, Match, etc.
+        }
+    }
+
+}
---
+++ SportsLeague.DataAccess/Repositories/TeamRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Interfaces.Repositories;
+
+namespace SportsLeague.DataAccess.Repositories;
+
+public class TeamRepository : GenericRepository<Team>, ITeamRepository
+{
+    public TeamRepository(LeagueDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Team?> GetByNameAsync(string name) //Aqui devuelvo un objeto de tipo team
+    {
+        return await _dbSet
+            .FirstOrDefaultAsync(t => t.Name.ToLower() == name.ToLower());
+    }
+
+    public async Task<IEnumerable<Team>> GetByCityAsync(string city) //aqui devuelvo una lista de objetos de tipo team, ya que puede haber varios equipos en la misma ciudad
+    {
+        return await _dbSet
+            .Where(t => t.City.ToLower() == city.ToLower())
+            .ToListAsync();
+    }
+}
